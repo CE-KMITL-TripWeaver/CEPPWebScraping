856cedd3af04d707d3612dbad1ab90ec05664bef
worachote1 <worachote659@gmail.com>
2024-07-21 03:11:57 +0700

    attraction_scraping: handle infinite scrolling

---
+++ packages/attraction_package/Attraction.py
@@ -0,0 +1,9 @@
+class Attraction:
+    def __init__(self) -> None:
+        self.name = ''
+        self.description = ''
+        self.latitude = 0
+        self.longitude = 0
+        self.imgPath = []
+        # defind rating object
+        
